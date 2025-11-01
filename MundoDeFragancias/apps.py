b701f71cffa0d4077a6ec59842af36c804dbfdbc
JJ190990 <exppjj@gmail.com>
2025-11-01 15:53:32 -0600

    Update apps.py

--- MundoDeFragancias/apps.py
+++ MundoDeFragancias/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MundodefraganciasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "MundoDeFragancias"
+    name = "mundodefragancias"
